aab08ee390376a11ebb9bcf35cdf9709cb4c502b
Natalie Davenport <natalie.davenport12@kzoo.edu>
2013-10-27 14:16:02 -0400

    Adding to skeleton

--- twentytwo/src/tootoo/twentytwo/MenuActivity.java
+++ twentytwo/src/tootoo/twentytwo/MenuActivity.java
@@ -1,23 +1,38 @@
 package tootoo.twentytwo;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MenuActivity extends Activity {
+	Context mContext = this;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_menu);
 
+		// Creates an intent to transfer the user to the EnterNames screen.
+		Button enterButton = (Button) findViewById(R.id.shittyButton);
+		enterButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(mContext, BioActivity.class);
+				startActivity(intent);
+			}
+		});
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu, menu);
+		return true;
+	}
+
 }
