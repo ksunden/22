431ebb0449d9f4a784ff2a9964923ec550c45c8c
Kyle Sunden <k10ks03@kzoo.edu>
2013-11-10 14:31:29 -0500

    store files

---
+++ twentytwo/src/tootoo/twentytwo/StoreContract.java
@@ -0,0 +1,31 @@
+package tootoo.twentytwo;
+
+import android.provider.BaseColumns;
+
+public class StoreContract{
+    
+    public StoreContract() {}
+    
+    // Define the column headings for a table of favorite sports
+    public static abstract class StoreItems implements BaseColumns
+    {
+        public static final String TABLE_NAME = "store_items";
+        public static final String COLUMN_NAME_NAME = "name";
+        public static final String COLUMN_NAME_PRICE = "price";
+        public static final String COLUMN_NAME_IMAGE_LOCATION = "image_location";
+        public static final String COLUMN_NAME_QUANTITY = "quantity";
+        
+        // String to create table
+        public static String SQL_CREATE_ITEMS = "CREATE TABLE " + TABLE_NAME + " (" +
+                                                    _ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                                                    COLUMN_NAME_NAME + " TEXT," +
+                                                    COLUMN_NAME_PRICE + " INTEGER,"+
+                                                    COLUMN_NAME_IMAGE_LOCATION + " TEXT,"+
+                                                    COLUMN_NAME_QUANTITY + " INTEGER)";
+        
+        // String to delete table
+        public static final String SQL_DELETE_ITEMS = "DROP TABLE IF EXISTS " + TABLE_NAME;
+        
+    }
+    
+}
---
+++ twentytwo/src/tootoo/twentytwo/StoreDbHelper.java
@@ -0,0 +1,37 @@
+package tootoo.twentytwo;
+
+import tootoo.twentytwo.StoreContract.StoreItems;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class StoreDbHelper extends SQLiteOpenHelper{
+    
+    // If you change the database schema, you must increment the database version.
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "StoreItems.db";
+    
+    public StoreDbHelper (Context c)
+    {
+        super(c, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL(StoreItems.SQL_CREATE_ITEMS);
+        
+        db.execSQL("INSERT INTO \"store_items\" VALUES (1, 'Wristbands (3 pack)', 10, null, 0);");
+        db.execSQL("INSERT INTO \"store_items\" VALUES (2, 'Wristband', 5, null, 0);");
+        db.execSQL("INSERT INTO \"store_items\" VALUES (3, 'Signed Puck', 22, null, 0);");
+        db.execSQL("INSERT INTO \"store_items\" VALUES (4, 'Signed Jersey', 222, null, 0);");
+                
+                
+    }
+    
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+        db.execSQL(StoreItems.SQL_DELETE_ITEMS);
+        onCreate(db);
+    }
+    
+}
