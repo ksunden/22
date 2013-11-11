10a56bc3a307c895e6c25d55f4def4feb5eac8bf
Natalie Davenport <natalie.davenport12@kzoo.edu>
2013-11-10 20:53:17 -0500

    Trying to work on terrible Twitter stuff

--- twentytwo/src/tootoo/twentytwo/SocialActivity.java
+++ twentytwo/src/tootoo/twentytwo/SocialActivity.java
@@ -1,5 +1,11 @@
 package tootoo.twentytwo;
 
+import java.util.List;
+
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
@@ -7,6 +13,8 @@ import android.view.Menu;
 import android.widget.ListView;
 
 public class SocialActivity extends Activity{
+    static String TWITTER_CONSUMER_KEY = "STUFFFFFF";
+    static String TWITTER_CONSUMER_SECRET = "Stufffffffffffff";
     
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -14,6 +22,26 @@ public class SocialActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social);
         
+        // TODO I have no idea how this will work and won't be able to get it to
+        // work for some time yet
+        
+        /*Twitter twitter = TwitterFactory.getSingleton();
+        twitter.setOAuthConsumer(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET);
+        List<Status> statuses = null;
+        try
+        {
+            statuses = twitter.getUserTimeline("TeamTooTooFund");
+        }catch(TwitterException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println("Showing home timeline.");
+        for(Status status : statuses)
+        {
+            System.out.println(status.getUser().getName() + ":" + status.getText());
+        }
+        */
         TwitterItem twitter_item_data[] = new TwitterItem[] {new TwitterItem(R.drawable.ic_launcher, "User 1", "1 Lorem Ipsum Dolor Sit Amet"), new TwitterItem(R.drawable.ic_launcher, "Person 2", "2 Lorem Ipsum Dolor Sit Amet")};
         TwitterItemAdapter adapter = new TwitterItemAdapter(this, R.layout.tweet_row, twitter_item_data);
         ListView lv = (ListView) findViewById(R.id.twitterListView);
