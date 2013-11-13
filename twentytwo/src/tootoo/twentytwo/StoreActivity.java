19df8fd6e1c87406a0064b9e100a4a5c7a8fdf86
Kyle Sunden <k10ks03@kzoo.edu>
2013-11-12 21:20:00 -0500

    Use image, updates to store

--- twentytwo/src/tootoo/twentytwo/StoreActivity.java
+++ twentytwo/src/tootoo/twentytwo/StoreActivity.java
@@ -3,17 +3,24 @@ package tootoo.twentytwo;
 import tootoo.twentytwo.StoreContract.StoreItems;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.SeekBar;
 import android.widget.SimpleCursorAdapter;
 import android.widget.SimpleCursorAdapter.ViewBinder;
 import android.widget.TextView;
@@ -25,29 +32,87 @@ public class StoreActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mContext = this;
-        SQLiteDatabase db = new StoreDbHelper(this).getWritableDatabase();
+        final SQLiteDatabase db = new StoreDbHelper(this).getWritableDatabase();
+        
         setContentView(R.layout.activity_store);
         Cursor cursor = db.query(StoreItems.TABLE_NAME, new String[]{StoreItems._ID,StoreItems.COLUMN_NAME_NAME}, null, null, null, null, null);
         GridView grid =(GridView) findViewById(R.id.store_grid);
         SimpleCursorAdapter itemAdapter = new SimpleCursorAdapter(this, R.layout.item_preview, cursor, new String[]
-                {StoreItems.COLUMN_NAME_NAME}, new int[]
-                        {R.id.preview_item_name});
+                {StoreItems.COLUMN_NAME_NAME, StoreItems._ID}, new int[]
+                        {R.id.preview_item_name, R.id.preview_tableId});
         itemAdapter.setViewBinder(new ViewBinder() {
             public boolean setViewValue(View view, Cursor cursor, int columnIndex)
             {
-                TextView tv = (TextView) view;
-                String str = cursor.getString(columnIndex);
-                tv.setText(str);
+                if(view instanceof TextView)
+                {
+                    TextView tv = (TextView) view;
+                    String str = cursor.getString(columnIndex);
+                    tv.setText(str);
+                }else if (view instanceof SeekBar)
+                {
+                    ((SeekBar) view).setProgress(cursor.getInt(columnIndex));
+                }
                 return true;
             }
         });
         grid.setAdapter(itemAdapter);
         grid.setOnItemClickListener(new OnItemClickListener() {
-
+            
             @Override
             public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3){
                 AlertDialog.Builder adb = new AlertDialog.Builder(mContext);
-                adb.setMessage(arg1.toString());
+                LayoutInflater inflater = getLayoutInflater();
+                final SeekBar id = (SeekBar) arg1.findViewById(R.id.preview_tableId);
+                
+                Cursor c = db.query(StoreItems.TABLE_NAME, null, "_id = "+id.getProgress(), null, null, null, null);
+                c.moveToFirst();
+                
+                View view = inflater.inflate(R.layout.item_detail,null);
+                adb.setView(view);
+                
+                TextView name = (TextView) view.findViewById(R.id.detail_name);
+                name.setText(c.getString(c.getColumnIndex(StoreItems.COLUMN_NAME_NAME)));
+                
+                TextView price = (TextView) view.findViewById(R.id.detail_price);
+                price.setText("$"+c.getInt(c.getColumnIndex(StoreItems.COLUMN_NAME_PRICE)));
+                
+                final EditText quantity = (EditText) view.findViewById(R.id.detail_quantity);
+                quantity.setText(""+c.getInt(c.getColumnIndex(StoreItems.COLUMN_NAME_QUANTITY)));
+                
+                Button minus = (Button) view.findViewById(R.id.detail_minus);
+                minus.setOnClickListener(new OnClickListener() {
+                    
+                    @Override
+                    public void onClick(View v){
+                        int curr = Integer.parseInt("0"+quantity.getText().toString());
+                        curr--;
+                        if (curr<0)
+                            curr=0;
+                        quantity.setText(""+curr);
+                    }
+                });
+                
+                Button plus = (Button) view.findViewById(R.id.detail_plus);
+                plus.setOnClickListener(new OnClickListener() {
+                    
+                    @Override
+                    public void onClick(View v){
+                        int curr = Integer.parseInt("0"+quantity.getText().toString());
+                        curr++;
+                        quantity.setText(""+curr);
+                    }
+                });
+                
+                adb.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+                    
+                    @Override
+                    public void onClick(DialogInterface dialog, int which){
+                        dialog.dismiss();
+                        ContentValues values = new ContentValues();
+                        values.put(StoreItems.COLUMN_NAME_QUANTITY, Integer.parseInt(quantity.getText().toString()));
+                        db.update(StoreItems.TABLE_NAME, values, "_id="+id.getProgress(), null);
+                    }
+                });
                 adb.create().show();
             }});
     }
@@ -61,7 +126,6 @@ public class StoreActivity extends Activity {
     
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
-        // TODO Auto-generated method stub
         if(item.getTitle().equals("Cart"))
         {
             Intent intent = new Intent(mContext, CartActivity.class);
@@ -69,5 +133,4 @@ public class StoreActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
-    
 }
--- twentytwo/src/tootoo/twentytwo/StoreDbHelper.java
+++ twentytwo/src/tootoo/twentytwo/StoreDbHelper.java
@@ -9,7 +9,7 @@ public class StoreDbHelper extends SQLiteOpenHelper{
     
     // If you change the database schema, you must increment the database version.
     public static final int DATABASE_VERSION = 1;
-    public static final String DATABASE_NAME = "StoreItems.db";
+    public static final String DATABASE_NAME = "tootoo.db";
     
     public StoreDbHelper (Context c)
     {
@@ -24,6 +24,7 @@ public class StoreDbHelper extends SQLiteOpenHelper{
         db.execSQL("INSERT INTO \"store_items\" VALUES (2, 'Wristband', 5, null, 0);");
         db.execSQL("INSERT INTO \"store_items\" VALUES (3, 'Signed Puck', 22, null, 0);");
         db.execSQL("INSERT INTO \"store_items\" VALUES (4, 'Signed Jersey', 222, null, 0);");
+        db.execSQL("INSERT INTO \"store_items\" VALUES (5, 'Hat', 15, null, 0);");
                 
                 
     }
