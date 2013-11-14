46b795d08a77176b239b9dda5ff603347bf18747
Natalie Davenport <natalie.davenport12@kzoo.edu>
2013-11-14 14:02:56 -0500

    Still working with the Twitter problem

--- twentytwo/src/tootoo/twentytwo/SocialActivity.java
+++ twentytwo/src/tootoo/twentytwo/SocialActivity.java
@@ -11,10 +11,8 @@ import twitter4j.TwitterException;
 import twitter4j.TwitterMethod;
 import twitter4j.conf.ConfigurationBuilder;
 import android.app.Activity;
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -24,8 +22,6 @@ public class SocialActivity extends Activity{
     static String TWITTER_CONSUMER_KEY = "Rtqg9HbxzxVb9Sp7T1Q";
     static String TWITTER_CONSUMER_SECRET = "zsMheHZcCIrVyLxDv3be9ocQ1D9XRDkyjVVBGkZVA";
     
-    static ContentValues values;
-    private static SQLiteDatabase database;
     private static ArrayList<TwitterItem> twitterList;
     private static TwitterItemAdapter adapter;
     
@@ -40,12 +36,6 @@ public class SocialActivity extends Activity{
         cb.setDebugEnabled(true).setOAuthConsumerKey(TWITTER_CONSUMER_KEY).setOAuthConsumerSecret(TWITTER_CONSUMER_SECRET).setOAuthAccessToken("1117933645-5KR4AJIvfQ9xvN0YTX28cJNoalk725VMFdOVLJg").setOAuthAccessTokenSecret("MgLqOiDqyMpYcQ53PE2Zg0paNWsuAxYilNREKfpIIqFqV");
         AsyncTwitter twitter = new AsyncTwitterFactory(cb.build()).getInstance();
         
-        // values = new ContentValues();
-        TweetDBHelper helper = new TweetDBHelper(this);
-        database = helper.getWritableDatabase();
-        
-        helper.onUpgrade(database, 0, 0);
-        
         twitter.addListener(new TwitterAdapter() {
             @Override
             public void gotUserTimeline(ResponseList<Status> statuses){
@@ -58,25 +48,6 @@ public class SocialActivity extends Activity{
                     TwitterItem item = new TwitterItem(R.drawable.ic_launcher, status.getUser().getName(), status.getText());
                     // twitterList.add(item);
                     adapter.add(item);
-                    // ContentValues vals = new ContentValues();
-                    // vals.put(TweetEntry.COLUMN_NAME_USER_NAME,
-                    // status.getUser().getName());
-                    // vals.put(TweetEntry.COLUMN_NAME_TWEET_CONTENT,
-                    // status.getText());
-                    
-                    // @SuppressWarnings("unused")
-                    // database.beginTransaction();
-                    // database.execSQL("INSERT INTO " + TweetEntry.TABLE_NAME +
-                    // "  (" + TweetEntry.COLUMN_NAME_TWEET_CONTENT + "," +
-                    // TweetEntry.COLUMN_NAME_USER_NAME + ") VALUES (" +
-                    // status.getText() + "," + status.getUser().getName() +
-                    // ");");
-                    // long newRowId =
-                    // database.insertOrThrow(TweetEntry.TABLE_NAME, null,
-                    // vals);
-                    // database.setTransactionSuccessful();
-                    // database.endTransaction();
-                    
                 }
                 adapter.notifyDataSetChanged();
                 super.gotUserTimeline(statuses);
@@ -101,23 +72,9 @@ public class SocialActivity extends Activity{
         }
         
         twitterList = new ArrayList<TwitterItem>();
-        // SQLiteCursor cursor = (SQLiteCursor)
-        // database.rawQuery("SELECT * FROM " + TweetEntry.TABLE_NAME, null);
-        // cursor.moveToFirst();
-        // while(!cursor.isAfterLast())
-        // {
-        // twitterItem.add(new TwitterItem(R.drawable.ic_launcher,
-        // cursor.getString(cursor.getColumnIndex(TweetEntry.COLUMN_NAME_USER_NAME)),
-        // cursor.getString(cursor.getColumnIndex(TweetEntry.COLUMN_NAME_TWEET_CONTENT))));
-        // cursor.moveToNext();
-        // }
-        
         adapter = new TwitterItemAdapter(this, R.layout.tweet_row, twitterList);
         ListView lv = (ListView) findViewById(R.id.twitterListView);
         lv.setAdapter(adapter);
-        
-        // database.close();
-        // cursor.close();
     }
     
     @Override
