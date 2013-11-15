c9e9df55b14ecf7035773855929f55e6a7dc7f32
davenportn <natalie.davenport12@kzoo.edu>
2013-11-15 15:55:30 -0500

    Got the main activity commented

--- src/com/example/android_sms/MainActivity.java
+++ src/com/example/android_sms/MainActivity.java
@@ -15,6 +15,16 @@ import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
 
+/**
+ * MainActivity is the primary activity for the 'No Texting While Driving' app.
+ * From this activity, the user can configure an auto-response message and
+ * enable speech-to-text for incoming messages. When transferring to the Driving
+ * activity, the user's message (possibly customized) will be saved and sent in
+ * response to any incoming text.
+ * 
+ * @author Kyle Sunden and Natalie Davenport
+ * @version 11/15/2013
+ */
 public class MainActivity extends Activity{
     private Context mContext = this;
     public static boolean isSpeechEnabled;
@@ -25,32 +35,41 @@ public class MainActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        isSpeechEnabled = false;
+        // Initially set the boolean for speech-to-text to true
+        isSpeechEnabled = true;
+        
+        // Auto-hide the keyboard
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
         
+        // Set the initial text for the user's custom response message
         final EditText message = (EditText) findViewById(R.id.message);
         message.setText("I'm currently driving and cannot answer your text");
         
+        // On click listener to take the user to the Driving activity
         Button driveButton = (Button) findViewById(R.id.startDriving);
         driveButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v){
+                // Set the static String to the contents of the custom message
+                // text field
                 responseText = message.getText().toString();
                 Intent intent = new Intent(mContext, DrivingActivity.class);
                 startActivity(intent);
             }
         });
         
+        // Change the static speech-to-text boolean
         CheckBox textToSpeach = (CheckBox) findViewById(R.id.speechEnabled1);
         textToSpeach.setChecked(MainActivity.isSpeechEnabled);
         textToSpeach.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
                 MainActivity.isSpeechEnabled = isChecked;
             }
         });
         
+        // Set up radio group listener and change the message based on the
+        // user's selection
         RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup1);
         rg.setOnCheckedChangeListener(new OnCheckedChangeListener() {
             @Override
