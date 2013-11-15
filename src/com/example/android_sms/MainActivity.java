ff7572b24837ec1d95130b5095ae10f93e1fd009
davenportn <natalie.davenport12@kzoo.edu>
2013-11-15 11:54:31 -0500

    Finished the layouts and radiogroups, message, isSpeechEnabled

--- src/com/example/android_sms/MainActivity.java
+++ src/com/example/android_sms/MainActivity.java
@@ -8,27 +8,53 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 
 public class MainActivity extends Activity{
     private Context mContext = this;
     public static boolean isSpeechEnabled;
+    public static String responseText;
     
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        // TODO set onClick listener to that check box for speech enabled
-        // in addition, check for radio option
-        // change message based on which radio button is
-        // Save driving message as the final string value for the thing
+        // TODO Save driving message as the final string value for the thing
+        
+        final EditText message = (EditText) findViewById(R.id.message);
+        message.setText("I'm currently driving and cannot answer your text");
         
         Button driveButton = (Button) findViewById(R.id.startDriving);
         driveButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v){
+                responseText = message.getText().toString();
                 Intent intent = new Intent(mContext, DrivingActivity.class);
                 startActivity(intent);
+            }
+        });
+        
+        RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup1);
+        rg.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId){
+                if(checkedId == R.id.radioDriving)
+                {
+                    message.setText("I'm currently driving and cannot answer your text");
+                }else if(checkedId == R.id.radioOffice)
+                {
+                    message.setText("I'm currently working and cannot answer your text");
+                }else if(checkedId == R.id.radioMeeting)
+                {
+                    message.setText("I'm currently in a meeting and cannot answer your text");
+                }else
+                {
+                    message.setText("");
+                    message.setHint("Enter your own message here");
+                }
             }
         });
     }
