0f237e8a8aa73016013957e33ff73aff891efdd6
Natalie Davenport <natalie.davenport12@kzoo.edu>
2013-11-15 10:59:44 -0500

    commiting this after I forgot to last night

--- src/com/example/android_sms/MainActivity.java
+++ src/com/example/android_sms/MainActivity.java
@@ -1,15 +1,36 @@
 package com.example.android_sms;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity{
+    private Context mContext = this;
+    public static boolean isSpeechEnabled;
     
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // TODO set onClick listener to that check box for speech enabled
+        // in addition, check for radio option
+        // change message based on which radio button is
+        // Save driving message as the final string value for the thing
+        
+        Button driveButton = (Button) findViewById(R.id.startDriving);
+        driveButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(mContext, DrivingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
     
     @Override
