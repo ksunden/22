1124e6dd256fd4e13d67d6a384cc629aa632e3f6
Kyle Sunden <k10ks03@kzoo.edu>
2013-11-20 20:42:46 -0500

    Resize images to prevent Out of Memory Exception

--- twentytwo/src/tootoo/twentytwo/StoreActivity.java
+++ twentytwo/src/tootoo/twentytwo/StoreActivity.java
@@ -10,7 +10,6 @@ import android.content.DialogInterface.OnCancelListener;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -58,9 +57,7 @@ public class StoreActivity extends Activity{
                 if(view instanceof ImageView)
                 {
                     ImageView iv = (ImageView) view;
-                    BitmapFactory.Options opts = new BitmapFactory.Options();
-                    opts.inSampleSize = 8;
-                    iv.setImageBitmap(BitmapFactory.decodeResource(getResources(), cursor.getInt(columnIndex), opts));
+                    iv.setImageResource(cursor.getInt(columnIndex));
                 }else if(view instanceof TextView)
                 {
                     // Bind name to the view
